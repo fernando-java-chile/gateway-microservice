a16c0a1358decd9b3e5f9e8c9ea30bb8a6ec2679
fernando-java-chile <fernando.java.chile@gmail.com>
2025-03-26 18:49:53 -0300

    Microservicios con Spring Boot - Autenticación y Autorización, Keycloak Server
    - Agregar el microservicio Hl7 Fhir Patient

--- src/main/java/com/ServiceGateway/api_gateway/security/SecurityConfiguration.java
+++ src/main/java/com/ServiceGateway/api_gateway/security/SecurityConfiguration.java
@@ -1,5 +1,39 @@
 package com.ServiceGateway.api_gateway.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated() // Todas las solicitudes deben estar autenticadas
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .authenticationSuccessHandler(authenticationSuccessHandler()) // Configura el manejador de éxito
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable); // Deshabilita CSRF
+
+        return http.build();
+    }
+
+    @Bean
+    public ServerAuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new RedirectServerAuthenticationSuccessHandler("/home"); // Redirige a "/home" después de la autenticación
+    }
+}
+
+
+/*
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,27 +44,22 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
-
 public class SecurityConfiguration {
-    // Intersectar todos los request y validar que el token sea correcto
 
-    // Define un valor predeterminado (true) si no se especifica en application.properties
     @Value("${security.enable-csrf:true}")
     private boolean csrfEnabled;
 
     @Bean
-    public SecurityWebFilterChain SecurityWebFilterChain(ServerHttpSecurity httpSecurity) throws Exception  {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults());
 
         if (!csrfEnabled) {
-            httpSecurity.csrf(csrf -> csrf.disable());
+            httpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
         }
 
-
         return httpSecurity.build();
-
-
     }
-
 }
+
+*/
